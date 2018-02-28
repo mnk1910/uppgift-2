6e42344fde4b8cfb6efa5835414670dd14548cb5
mnk1910 <mnk1910@local>
2018-02-28 11:25:54 +0100

    Refactor the code to use mysql as database
    * add a "log order" feature
    * prevent loading this page directly
    * update the form "order"
    * other minor changes

--- order.php
+++ order.php
@@ -1,7 +1,7 @@
 <?php
 
 $title = "Checkout form";
-include ("./includes/top.php");
+include ("./includes/header.php");
 
 $error_get = '';
 
@@ -9,35 +9,15 @@ if (!isset($_GET['parameter']) || empty($_GET['parameter'])){
     $error_get = 'Incorrect parameter, please try again. ';
 }
 
-$decoded = base64_decode($_GET['parameter']);
-$parameters = unserialize($decoded);
-
-$product_name = $parameters['0'];
-$img = $parameters['1'];
-$product_id = $parameters['2'];
-$product_description = $parameters['3'];
-$product_price = $parameters['4'];
+if (!isset($_SERVER['HTTP_REFERER']) || $_SERVER['HTTP_REFERER'] != $http_referer){
+    $error_get .= 'You can not load this page directly. ';
+}
 
 if (isset($_POST['action']) && ($_POST['action'] == 'update')){
 $error_post = '';
 
-if (!isset($_POST['customer_name']) || empty ($_POST['customer_name']) || !isset($_POST['customer_mail']) || empty ($_POST['customer_mail']) || !isset($_POST['customer_address']) || empty ($_POST['customer_address'])){
-    $error_post = 'Error processing the billing form. ';
-}
-
-$to = $_POST['customer_mail'];
-$subject = 'Order placed by ' . $_POST['customer_name'];
-$headers = "From: vrabie@inariptata.com\r\n" . "X-Mailer: php";
-$headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-$headers .= 'Bcc: ilnitchi@gmail.com';
-$message = '<html><body>';
-$message .= 'New order for the customer <strong>' . $_POST['customer_name'] . "</strong><br /><br />\n";
-$message .= '<strong>Product</strong>: <br />' . $product_name . ' (article id: ' . $product_id . ') ' . "<br /><br />\n";
-$message .= '<strong>Product description</strong>: <br />' . $product_description . "<br /><br />\n";
-
-if (!mail($to , $subject, $message, $headers)){
-    $error_post .= 'Error sending the customer the confirmation email.';
+if (!isset($_POST['customer_name']) || empty ($_POST['customer_name']) || !isset($_POST['customer_mail']) || empty ($_POST['customer_mail']) || !isset($_POST['customer_address']) || empty ($_POST['customer_address']) || !isset($_POST['customer_phone']) || empty ($_POST['customer_phone'])){
+    $error_post = 'Error processing the checkout form. ';
 }
 
 if ($error_post){
@@ -46,28 +26,54 @@ if ($error_post){
           <div class="card flex-md-row mb-4 h-md">
             <div class="card-body d-flex flex-column">
               <h3 class="mb-0">
-                <strong class="d-inline-block mb-2 text-danger"><?php echo $error_post; ?></strong>
+                <div class="alert alert-danger" role="alert"><?=$error_post ?></div>
+                <!-- <strong class="d-inline-block mb-2 text-danger"><?=$error_post ?></strong> -->
               </h3>
             </div>
           </div>
     </div>
     <?php
     } else {
+        // no errors found, order is complete!
 ?>
 
 <div class="jumbotron p-3 p-md-5 rounded">
       <div class="card flex-md-row mb-4 h-md">
         <div class="card-body d-flex flex-column">
           <h3 class="mb-0">
-            <strong class="d-inline-block mb-2 text-success">Your order has been successfully placed!</strong>
+            <div class="alert alert-success" role="alert">Your order has been successfully placed!</div>
+            <!-- <strong class="d-inline-block mb-2 text-success">Your order has been successfully placed!</strong> -->
           </h3>
+          <div class="container">
+              <p class="float-left">
+              <a href="<?=$http_referer?>" class="btn btn-outline-success">Go to the index page.</a>
+              </p>
+          </div>
+          <?php
+          // use the available data (GET and POST) and insert it into the database
+          // note: $_GET is serialized and encoded.
+          // print_r ($_GET);
+          // print_r ($_POST);
+
+          $decoded = base64_decode($_GET['parameter']);
+          $parameters = unserialize($decoded);
+
+          $product_id = $parameters['product_id'];
+
+          $query_order_log = "INSERT INTO order_log SET product_id='" . $product_id . "', customer_name='" . $_POST['customer_name'] . "', customer_mail='" . $_POST['customer_mail'] . "', customer_phone='" . $_POST['customer_phone'] . "', customer_address='" . $_POST['customer_address'] . "'";
+          // check the generated SQL INSERT query
+          // echo $query_order_log;
+          $sql_order_log = mysqli_query($connection, $query_order_log);
+          ?>
         </div>
       </div>
 </div>
 
 
 <?php
-    }
+        include ("./includes/footer.php");
+        exit;
+    } // end else
 }
 
 if ($error_get){
@@ -76,7 +82,8 @@ if ($error_get){
       <div class="card flex-md-row mb-4 h-md">
         <div class="card-body d-flex flex-column">
           <h3 class="mb-0">
-            <strong class="d-inline-block mb-2 text-danger"><?php echo $error_get; ?></strong>
+            <div class="alert alert-danger" role="alert"><?=$error_get ?></div>
+            <!-- <strong class="d-inline-block mb-2 text-danger"><?=$error_get ?></strong> -->
           </h3>
         </div>
       </div>
@@ -85,6 +92,19 @@ if ($error_get){
 }
 else{
 
+    $decoded = base64_decode($_GET['parameter']);
+    $parameters = unserialize($decoded);
+
+    // echo "<pre>";
+    // print_r($_GET);
+    // echo "</pre>";
+
+    $product_name = $parameters['name'];
+    $img = $parameters['image'];
+    $product_id = $parameters['product_id'];
+    $product_description = $parameters['description'];
+    $product_price = $parameters['price'];
+
 ?>
 <div class="jumbotron p-3 p-md-5 rounded">
           <div class="card flex-md-row mb-4 h-md">
@@ -92,37 +112,46 @@ else{
               <h3 class="mb-0">
                 <strong class="d-inline-block mb-2 text-primary"><?php echo $product_name; ?></strong>
               </h3>
-              <div class="mb-2 text-muted">Product Nr: <?php echo $product_id; ?></div>
-              <p class="card-text mb-auto"><?php echo $product_description; ?></p>
+              <div class="mb-2 text-muted">Product Nr: <?=$product_id ?></div>
+              <div class="card-title pricing-card-title">Price: <?=$product_price ?> <small class="text-muted">kr</small></div>
+              <p class="card-text mb-auto"><?=$product_description ?></p>
             </div>
-            <img class="card-img-right flex-auto d-none d-md-block" src="./images/<?php echo $img; ?>" src="holder.js/200x250?theme=thumb" alt="Card image cap">
+            <img class="card-img-right flex-auto d-none d-md-block" src="./images/<?=$img ?>" src="holder.js/200x250?theme=thumb" alt="Card image cap">
           </div>
 </div>
-
+<?php
+// $_SERVER['PHP_SELF'] - current opened file
+?>
 <h2 class="mb-2">To finalize your order, please fill in the following form:</h2>
-    <form action="<?php echo $_SERVER['PHP_SELF']?>?parameter=<?php echo $_GET['parameter']; ?>" method="POST" name="order">
+    <form action="<?=$_SERVER['PHP_SELF'] ?>?parameter=<?=$_GET['parameter'] ?>" method="POST" name="order">
         <div class="row">
             <div class="col-md-6 mb-3">
                     <label for="Name">Name</label>
-                    <input type="text" class="form-control" id="customer_name" name="customer_name" placeholder="Your name" value="" required="">
+                    <input type="text" class="form-control" id="customer_name" name="customer_name" placeholder="Your name" value="" required>
             </div>
         </div>
         <div class="row">
             <div class="col-md-6 mb-3">
                     <label for="Email">Email</label>
-                    <input type="email" class="form-control" id="customer_mail" name="customer_mail" placeholder="Your email" value="" required="">
+                    <input type="email" class="form-control" id="customer_mail" name="customer_mail" placeholder="Your email" value="" required>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 mb-3">
+                    <label for="Phone number">Phone number</label>
+                    <input type="text" class="form-control bfh-phone" data-country="SV" id="customer_phone" name="customer_phone" placeholder="Your phone number" value="" required>
             </div>
         </div>
         <div class="row">
             <div class="col-md-6 mb-3">
                     <label for="Address">Delivery address</label>
-                    <input type="address" class="form-control" id="customer_address" name="customer_address" placeholder="Address" value="" required="">
+                    <input type="address" class="form-control" id="customer_address" name="customer_address" placeholder="Your address" value="" required>
             </div>
         </div>
         <div class="row">
             <div class="col-md-6 mb-3">
                     <label for="Message">Message</label>
-                    <textarea cols="50" rows="5" type="text" class="form-control" id="customer_message" name="customer_message" placeholder="Message"></textarea>
+                    <textarea cols="50" rows="5" type="text" class="form-control" id="customer_message" name="customer_message" placeholder="Your message"></textarea>
             </div>
         </div>
         <div class="row">
@@ -137,6 +166,7 @@ else{
 <?php
 }
 
-include ("./includes/bottom.php");
+
+include ("./includes/footer.php");
 
 ?>
